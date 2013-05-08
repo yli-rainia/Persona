e0bd4c02eb463263c62a750ddca0cbbb599d7703
Yanzheng Li <yli@rainia.org>
2013-05-08 17:58:50 +0000

    Updated toybox/sqlalchemy_rds_conn.py

--- toybox/sqlalchemy_rds_conn.py
+++ toybox/sqlalchemy_rds_conn.py
@@ -4,10 +4,11 @@ import sqlalchemy
 def main():
   print 'attempt to connect to rds mysql db'
 
-  db = sqlalchemy.create_engine(
+  engine = sqlalchemy.create_engine(
     'mysql://yli:mylifeforhire@persona-testing.c9urfdd69kod.us-west-1.rds.amazonaws.com/persona_testing',
-    echo=True
+    echo=False
   )
+  engine.execute("select 1").scalar()
   print 'done..'
   
 
