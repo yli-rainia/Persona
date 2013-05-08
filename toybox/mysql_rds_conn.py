f50a2278cf3d7f036296e194eb339d2482493b77
Yanzheng Li <yli@rainia.org>
2013-05-08 15:13:01 +0000

    Updated toybox/mysql_rds_conn.py

--- toybox/mysql_rds_conn.py
+++ toybox/mysql_rds_conn.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
+import sys
 import _mysql
 
+"""
+Checks connection to MySQL instance on AWS RDS.
+"""
+
+DB_NAME='persona_testing'
+
+
 def main():
-  print 'attempting to connect to db persona_testing'
+  sys.stdout.write('Checking connection to RDS MySQL instance %s: ' % DB_NAME)
 
   con = None
   try:
@@ -10,15 +18,16 @@ def main():
       'persona-testing.c9urfdd69kod.us-west-1.rds.amazonaws.com',
       'yli',
       'mylifeforhire',
-      'persona_testing'
+      DB_NAME
     )
+    sys.stdout.write('GOOD')
   except _mysql.Error, e:
-    print "Error %d: %s" % (e.args[0], e.args[1])
+    sys.stdout.write("Error %d: %s" % (e.args[0], e.args[1]))
   finally:
     if con:
       con.close()
 
-  print 'done...'
+  sys.stdout.write('\n')
 
 
 if __name__ == '__main__':
